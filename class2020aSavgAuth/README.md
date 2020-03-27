2bbc411c0e04e6d291d2a2a830c9a16a2b66c4b3
Nyah Macklin <60084237+MacklinEngineering@users.noreply.github.com>
2020-03-27 10:29:31 -0400

    Add files via upload

---
+++ class2020aSavgAuth/README.md
@@ -0,0 +1,3 @@
+# class2020aSavgAuth
+ 
+As for our detailed and unified documentation, please refer to : https://docs.google.com/document/d/1c8-fnRssGUvw9SVgectG4YDk1h1-B_6fIuwjqfmmCzA/edit?usp=sharing
---
+++ class2020aSavgAuth/config/database.js
@@ -0,0 +1,9 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://demo:admin@communalappcluster-sjgxl.mongodb.net/test?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'COMMMUNAL_APP_DATABASE_NAME'
+
+    //COLLECTION NAME: ITEM_POSTS_TO_LISTINGS
+    //COLLECTION NAME: USERS
+};
